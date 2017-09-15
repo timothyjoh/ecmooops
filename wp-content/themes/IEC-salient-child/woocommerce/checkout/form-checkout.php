7a1e3d9b7bde585ba935f84ed3f4a12193cc5432
Peter Malcolm <peter.t.malcolm@gmail.com>
2017-09-15 13:49:08 -0400

    improved /checkout/ layout

--- wp-content/themes/IEC-salient-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/IEC-salient-child/woocommerce/checkout/form-checkout.php
@@ -36,21 +36,17 @@ $get_checkout_url = apply_filters( 'woocommerce_get_checkout_url', WC()->cart->g
 			</div>
 
 			<div class="col-2">
-				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+				<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
+				<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+				<div id="order_review" class="woocommerce-checkout-review-order">
+					<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+				</div>	
 			</div>
 		</div>
 
 		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-		<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
-
 	<?php endif; ?>
-
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
 
 	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
 
